0ff8ee9dd3a062df950fead2354b786f629a2481
Jannu Rey Basarte <truegamer12120405@gmail.com>
2025-09-23 16:17:25 +0800

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+// at the top of src/App.tsx
+import { inject } from '@vercel/analytics';
+inject();
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
